e7f5984f8f890ba232e655afca0a8d26cb45b292
MAOIRO17 <maoiro17@gmail.com>
2025-08-13 19:14:59 +0200

    Ejercicio Simple de una tienda online para prácticar el uso de más de  un componente con Angular

--- AppAngular/src/app/app.ts
+++ AppAngular/src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
 import { ComponenteEnLinea } from "./componente-en-linea/componente-en-linea";
 import { Interpolacion } from "./interpolacion/interpolacion";
+import { Padre } from './padre/padre';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [NuevoComponente, ComponenteEnLinea, Interpolacion]
+  imports: [NuevoComponente, ComponenteEnLinea, Interpolacion, Padre]
 })
 export class App {  
-  title = signal('Hola Mundo');
+  title = signal('Curso Angular');
 }
