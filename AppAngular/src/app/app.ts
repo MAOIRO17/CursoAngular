6eb3c377d4d748478a91837143bd0a86f4578e18
MAOIRO17 <maoiro17@gmail.com>
2025-08-13 17:51:52 +0200

    Creación de un ejemplo de componente en Angular

--- AppAngular/src/app/app.ts
+++ AppAngular/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [NuevoComponente]
 })
 export class App {
   title = signal('Hola Mundo');
