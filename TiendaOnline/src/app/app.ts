b10802dc65902569b1d605826efc988b431b6d8d
MAOIRO17 <maoiro17@gmail.com>
2025-08-14 18:30:02 +0200

    Ejercicio Simple de una tienda online con listado de productos con Angular

--- TiendaOnline/src/app/app.ts
+++ TiendaOnline/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListadoProductos } from "./listado-productos/listado-productos";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ListadoProductos],
+  imports: [RouterOutlet, ListadoProductos,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = ('TiendaOnline');
+  title = ('Tienda Online');
 }
--- TiendaOnline/src/app/listado-productos/listado-productos.ts
+++ TiendaOnline/src/app/listado-productos/listado-productos.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { Producto } from "./producto/producto";
+import { Producto } from '../Producto';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listado-productos',
-  imports: [Producto],
+  standalone: true, // ✅ standalone
+  imports: [CommonModule, FormsModule], 
   templateUrl: './listado-productos.html',
-  styleUrl: './listado-productos.css'
+  styleUrls: ['./listado-productos.css']
 })
 export class ListadoProductos {
+  productos: Producto[] = [
+    new Producto('Camiseta Angular', 20),
+    new Producto('Pantalón React', 30),
+    new Producto('Zapatos Vue', 50)
+  ];
 
+  descripcionInput: string = '';
+  precioInput: number | null = null;
+
+  agregarProducto() {
+    if (
+      this.descripcionInput.trim() === '' ||
+      this.precioInput == null ||
+      this.precioInput <= 0
+    ) {
+      alert('Por favor, ingrese una descripción válida y un precio mayor a 0.');
+      return;
+    }
+
+    const nuevoProducto = new Producto(this.descripcionInput, this.precioInput);
+    this.productos.push(nuevoProducto);
+    this.descripcionInput = '';
+    this.precioInput = null;
+  }
 }
