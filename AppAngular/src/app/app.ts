8f7066f2ca02755607533b3a6531f3eccf3bf1f6
MAOIRO17 <maoiro17@gmail.com>
2025-08-14 12:55:21 +0200

    Ejercicio de Event Binding en Angular

--- AppAngular/src/app/app.ts
+++ AppAngular/src/app/app.ts
@@ -4,12 +4,13 @@ import { ComponenteEnLinea } from "./componente-en-linea/componente-en-linea";
 import { Interpolacion } from "./interpolacion/interpolacion";
 import { Padre } from './padre/padre';
 import { Hijo } from "./hijo/hijo";
+import { MostrarMensaje } from './mostrar-mensaje/mostrar-mensaje';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [NuevoComponente, ComponenteEnLinea, Interpolacion, Padre, Hijo]
+  imports: [NuevoComponente, ComponenteEnLinea, Interpolacion, Padre, Hijo,MostrarMensaje]
 })
 export class App {  
   title = signal('Curso Angular');
