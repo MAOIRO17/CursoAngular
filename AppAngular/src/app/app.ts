cbaca69aa5c6350716f6199e4dfd2a0c0c0b419d
MAOIRO17 <maoiro17@gmail.com>
2025-08-14 13:09:07 +0200

    Ejercicio de Property Binding en Angular

--- AppAngular/src/app/app.ts
+++ AppAngular/src/app/app.ts
@@ -5,13 +5,14 @@ import { Interpolacion } from "./interpolacion/interpolacion";
 import { Padre } from './padre/padre';
 import { Hijo } from "./hijo/hijo";
 import { MostrarMensaje } from './mostrar-mensaje/mostrar-mensaje';
+import { Replicador } from './replicador/replicador';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [NuevoComponente, ComponenteEnLinea, Interpolacion, Padre, Hijo,MostrarMensaje]
+  imports: [NuevoComponente, ComponenteEnLinea, Interpolacion, Padre, Hijo,MostrarMensaje,Replicador]
 })
 export class App {  
-  title = signal('Curso Angular');
+  title = signal('Property Binding');
 }
