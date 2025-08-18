22a1336237bc4227e4327db831fb4569e7e3b6b5
MAOIRO17 <maoiro17@gmail.com>
2025-08-18 18:26:56 +0200

    Ejercicio de Decorador ViewChild en Angular

--- AppAngular/src/app/app.ts
+++ AppAngular/src/app/app.ts
@@ -4,13 +4,14 @@ import { ComponenteFor } from "./componente-for/componente-for";
 import { ComponenteIf } from './componente-if/componente-if';
 import { Hijo } from './padre/hijo/hijo';
 import { Padre } from './padre/padre';
+import { ViewChildComponent } from "./view-child/view-child";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [ComponenteIf, AgregarTarea, ComponenteFor, Padre,Hijo]
+  imports: [ComponenteIf, AgregarTarea, ComponenteFor, Padre, Hijo, ViewChildComponent]
 })
 export class App {  
-  title = "Output en Angular"; ;
+  title = "Decorador ViewChild en Angular"; ;
 }
